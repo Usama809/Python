5de212d4986d684428258f188a228580f16e49a9
Usama Bin Amjad <mohammadosama6666@gmail.com>
2025-08-26 22:24:54 +0500

    modules learning

--- Fundamental/Operators.py
+++ Fundamental/Operators.py
@@ -1,6 +1,11 @@
+ # Arithmatic Operators
+
 a = 12
 b = 20 
-print (a+b);
-print (a-b);
-print (a*b);
-print (a/b);
+print (a+b)
+print (a-b)
+print (a*b)  
+print (a/b)   #floar devision (Show only 4.0, dont remove float values)
+print (a//b)  #floar devision (remove flaot part like 4.0. Show only 0)
+print (a**b)  #exponential (use for powers)
+print (a%b)   #modules (Provide the reminder)
