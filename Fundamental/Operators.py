bc9c780ee5f511e887b125761b25b0d41d4ea07b
Usama Bin Amjad <mohammadosama6666@gmail.com>
2025-08-27 21:10:45 +0500

    Comparison & Logical Operators

--- Fundamental/Operators.py
+++ Fundamental/Operators.py
@@ -1,11 +1,42 @@
- # Arithmatic Operators
+# Arithmatic Operators
 
 a = 12
-b = 20 
-print (a+b)
-print (a-b)
-print (a*b)  
-print (a/b)   #floar devision (Show only 4.0, dont remove float values)
-print (a//b)  #floar devision (remove flaot part like 4.0. Show only 0)
-print (a**b)  #exponential (use for powers)
-print (a%b)   #modules (Provide the reminder)
+b = 20
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)  # floar devision (Show only 4.0, dont remove float values)
+print(a//b)  # floar devision (remove flaot part like 4.0. Show only 0)
+print(a**b)  # exponential (use for powers)
+print(a % b)  # modules (Provide the reminder)
+
+
+# Compound Assignment Operators
+c = 40
+d = 50
+c += 40
+c += 40
+
+c -= 40
+c *= 40
+c /= 40
+c //= 40
+c**= 40
+print (c)
+
+# Camparison Operators
+
+e = 12
+f = 20
+print(e == f)
+print (e != f)
+print (e > f)
+print (e < f)
+print (e <= f)
+print (e >= f)
+
+
+# Logical Operator
+
+print (12 > 10 and 30 == 30 )
+print (12 < 140 or 40 == 40)
