8151711136399c41cca7b7094bd93e799a8f2660
Usama Bin Amjad <mohammadosama6666@gmail.com>
2025-08-28 10:59:15 +0500

    Loops Learning

---
+++ Fundamental/Loops.py
@@ -0,0 +1,28 @@
+# In for loops we set some range funcitons which start form 0 then stop then steps (S,S,S) => Start , Stop , Steps
+
+# for i in range (1, 20, 1):
+#     print (i)
+
+# n = int(input ("Which Table you want to print"))
+# for i in range(n ,(n*10)+1 , n):
+#     print (i);
+
+# Strings for Loops
+#  if we use string then it's index form zero but if we check lenght then it's start from 1 
+
+# a = "Usama Teaches Programming in university"
+# print (len(a))
+
+
+# for i in range(len(a)):
+#     print(a[i])
+
+
+# Break in statement loops 
+
+for i in range (1 , 20):
+    print (i)
+    if i == 15:
+        break
+    else:
+        print(i)
