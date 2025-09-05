1e9cbf2ee20bc3f68e0a0f7b58036b35ab5f8481
Usama Bin Amjad <mohammadosama6666@gmail.com>
2025-09-05 16:01:47 +0500

    Tuples are cleared

---
+++ Data_Structures/Tuple.py
@@ -0,0 +1,19 @@
+# Tuples are mutable ,Duplicate, hetrogenous nature 
+# a = (1,2,3,4)
+# print(a[2])
+
+# There are only 2 methods 1) index 2) count
+
+# a = (1,3,4,6 , print() , "hello")
+# index = a.index(4)
+# print(index)
+
+# a = (1,3,4,6 ,6 , print() , "hello")
+# count = a.count(6)
+# print(count)
+
+
+# a = (1 ,)  #this one behaves like a tuple
+
+# print(type(a))
+
