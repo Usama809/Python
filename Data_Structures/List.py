2171dd691a2d8c967fdeadf4e50de868cb45b7a2
Usama Bin Amjad <mohammadosama6666@gmail.com>
2025-09-05 14:37:21 +0500

    List problem Solving

--- Data_Structures/List.py
+++ Data_Structures/List.py
@@ -5,10 +5,32 @@
 #     print(a[i])
 
 
-b = [1 ,2, 3]
+#b = [1 ,2, 3]
 # b.insert(1, 10) => [1, 10, 2, 3]
 # b.append(5) => [1, 2, 3, 5]
-b.extend([5 , 30 , 60])
+# b.extend([5 , 30 , 60]) => [1, 2, 3, 5, 30, 60]
+
+#print (b)
+
+# Q1 -  Print positive and negative elements of an List?
+
+# l = [1,2,-3,4,5, -1 ,-4 -3 ]
+# print("pPrint Positive Numbers")
+
+# for i in l:
+#     if i >= 0:
+#         print(i)
+
+# print("Print Negative Numbers")
+# for i in l:
+#     if i < 0:
+#         print(i)
 
 
-print (b)
+l = [12,2,43,56]
+sum = 0
+for i in l:
+    sum = sum + i
+
+print(sum/len(l))
+
