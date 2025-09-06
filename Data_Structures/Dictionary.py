894d38714e62b8d95b2a9e9adf01b79add0644ba
Usama Bin Amjad <mohammadosama6666@gmail.com>
2025-09-06 15:31:56 +0500

    Deep copy and Shallow Copy

--- Data_Structures/Dictionary.py
+++ Data_Structures/Dictionary.py
@@ -5,13 +5,24 @@
 # print(type(a))
 
 
-d ={10 : 100 , 20 : 2000 , 30 : 3000}
+# d ={10 : 100 , 20 : 2000 , 30 : 3000}
 # d[10] = 1000
 # d.update({40 : 4000})
 
-d[10] = 1000 #updating
-d[50]  = 5000 #updating
+# d[10] = 1000 #updating
+# d[50]  = 5000 #creating 
+# del d[30] # delete
+# print(d)
 
-del d[30]
-print(d)
+# Dictonary Traversing
 
+# d = {10 : 100 , 20 : 2000 , 30 : 3000}
+
+# for i in d.values():
+#     print(i)
+
+a =[1,2,3,4]
+
+b = a
+b[0] = 100
+print (b) 
